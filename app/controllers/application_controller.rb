dfe09867aeaedc1603a109cdd6f7188b07541b8e
ryosuke.kazama <chipdale0119@gmail.com>
2020-01-15 12:45:11 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to root_path
+    end
+  end
 end
