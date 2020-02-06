5a937fb2fc98ea825ad144ad3af952c144e3c148
ryosuke.kazama <chipdale0119@gmail.com>
2020-02-06 05:15:50 +0000

    confirm

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User.create!(name: "ユーザー10", email: "user10@exsample.com", password: "user10", password_confirmation: "user10", profile: "ユーザー10です")
 
 10.times do |n|
-  name = "Exsample#{n+59}"
+  name = "Exsample#{n+100}"
   genre = rand(1..12)
   target = rand(1..3)
-  comment = "お試しルーム#{n+59}。"
-  user_id = 7
+  comment = "お試しルーム#{n+100}。"
+  user_id = 2
   Chatroom.create!(name: name, genre: genre, target: target, comment: comment, user_id: user_id)
 end
